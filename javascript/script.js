853c0835bae372f61143429661fd9ed93fb1b4c5
SimonGideon <simongideon918@gmail.com>
2022-10-20 00:15:56 +0300

    Fix linter errors

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,19 @@
-const classNav = document.getElementById("nav");
-const openNav = document.getElementById("hamburger");
-const closeNav = document.getElementById("close");
-const navItem1 = document.getElementById("navItem1");
-const navItem2 = document.getElementById("navItem2");
-const navItem3 = document.getElementById("navItem3");
-const navItem4 = document.getElementById("navItem4");
-const navItem5 = document.getElementById("navItem5");
+const classNav = document.getElementById('nav');
+const openNav = document.getElementById('hamburger');
+const closeNav = document.getElementById('close');
+const navItem1 = document.getElementById('navItem1');
+const navItem2 = document.getElementById('navItem2');
+const navItem3 = document.getElementById('navItem3');
+const navItem4 = document.getElementById('navItem4');
+const navItem5 = document.getElementById('navItem5');
 
 // open-nav
-function showSideBar(){
-    classNav.classList.add('nav');
+function showSideBar() {
+  classNav.classList.add('nav');
 }
 // close-nav
-function closeSideBar(){
-    classNav.classList.remove('nav');
+function closeSideBar() {
+  classNav.classList.remove('nav');
 }
 
 openNav.addEventListener('click', showSideBar);
